375e41b066c930bdda74de0ce952e9a776529ce9
spennington45 <spennington45@gmail.com>
2020-09-27 16:58:22 -0500

    seperate into css and js with some tweeks

--- js/script.js
+++ js/script.js
@@ -2,7 +2,57 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
     zoom: 8,
   });
+}
+
+"use strict";
+
+function initMap() {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: {
+      lat: 41.85,
+      lng: -87.65,
+    },
+    disableDefaultUI: true,
+  });
+  directionsRenderer.setMap(map);
+
+  var onChangeHandler = function onChangeHandler() {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+
+  document
+    .getElementById("start")
+    .addEventListener("change", onChangeHandler);
+  document
+    .getElementById("end")
+    .addEventListener("change", onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService.route({
+      origin: {
+        query: document.getElementById("start").value,
+      },
+      destination: {
+        query: document.getElementById("end").value,
+      },
+      travelMode: google.maps.TravelMode.DRIVING,
+    },
+    function(response, status) {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
 }
